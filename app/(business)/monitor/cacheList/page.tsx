480159bd31ed120e4bc61499511d8a3e5673c01c
dev2007 <softengineerwu@gmail.com>
2024-02-20 20:43:44 +0800

    开始缓存列表页面

--- app/(business)/monitor/cacheList/page.tsx
+++ app/(business)/monitor/cacheList/page.tsx
@@ -1,3 +1,91 @@
-export default function CacheList(){
-    return <div>cachelist</div>
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { ReloadOutlined, ClearOutlined } from "@ant-design/icons";
+import { Row, Col, Tooltip } from "antd";
+import { PageContainer, ProCard } from "@ant-design/pro-components";
+import {
+  faFloppyDisk,
+  faKey,
+  faFileLines,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fetchApi } from "@/app/_modules/func";
+
+export default function CacheList() {
+  const { push } = useRouter();
+
+  return (
+    <PageContainer title={false}>
+      <Row gutter={8}>
+        <Col span={8}>
+          <ProCard
+            title={
+              <>
+                <FontAwesomeIcon icon={faFloppyDisk} />
+                <span style={{ marginLeft: 6 }}>缓存列表</span>
+              </>
+            }
+            extra={
+              <Tooltip title="刷新">
+                <a>
+                  <ReloadOutlined />
+                </a>
+              </Tooltip>
+            }
+            headerBordered
+            bordered
+            hoverable
+          >
+            1
+          </ProCard>
+        </Col>
+        <Col span={8}>
+          <ProCard
+            title={
+              <>
+                <FontAwesomeIcon icon={faKey} />
+                <span style={{ marginLeft: 6 }}>键名列表</span>
+              </>
+            }
+            extra={
+              <Tooltip title="刷新">
+                <a>
+                  <ReloadOutlined />
+                </a>
+              </Tooltip>
+            }
+            headerBordered
+            bordered
+            hoverable
+          >
+            2
+          </ProCard>
+        </Col>
+        <Col span={8}>
+          <ProCard
+            title={
+              <>
+                <FontAwesomeIcon icon={faFileLines} />
+                <span style={{ marginLeft: 6 }}>缓存内容</span>
+              </>
+            }
+            extra={
+              <Tooltip title="清空">
+                <a>
+                  <ClearOutlined />
+                </a>
+              </Tooltip>
+            }
+            headerBordered
+            bordered
+            hoverable
+          >
+            3
+          </ProCard>
+        </Col>
+      </Row>
+    </PageContainer>
+  );
 }
